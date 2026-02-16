dcd2b89ed4af9ebf1e6ef891c65a636bbe2d18e1
ploytanat <ploythanat1@gmail.com>
2026-02-16 13:27:48 +0700

    Update wp.ts

--- app/lib/api/wp.ts
+++ app/lib/api/wp.ts
@@ -1,11 +1,19 @@
 const WP = process.env.WP_API_URL;
 
+if (!WP) {
+  throw new Error("WP_API_URL is not defined");
+}
+
 export async function getPosts() {
   const res = await fetch(`${WP}/wp-json/wp/v2/posts`, {
     next: { revalidate: 60 },
   });
 
-  if (!res.ok) throw new Error("Failed to fetch WP");
+  if (!res.ok) {
+    throw new Error("Failed to fetch WordPress posts");
+  }
 
   return res.json();
+  
 }
+console.log("WP_API_URL:", process.env.WP_API_URL);
